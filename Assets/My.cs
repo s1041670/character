3865080ab46e05acb2e3086820273e15b713cb87
cenue <cen.ue86@gmail.com>
2017-04-20 19:09:41 +0800

    walk

---
+++ Assets/My.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class My : MonoBehaviour {
+    Animator anim;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("up") || Input.GetButton("Jump"))
+        {
+            // ブーリアンNextをtrueにする
+            anim.SetBool("next", true);
+        }
+    }
+}
